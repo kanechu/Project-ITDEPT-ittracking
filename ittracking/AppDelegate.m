c688937f96693fe231332cc38fd20575e75395dd
kanechu <kanechu@itdept.com.hk>
2014-08-09 11:23:09 +0800

    remove warning

--- ittracking/AppDelegate.m
+++ ittracking/AppDelegate.m
@@ -19,7 +19,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     // Override point for customization after application launch.
-     DB_login *dbLogin=[[DB_login alloc]init];
     NSLog(@"Registering for push notifications...");
     [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
      (UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge)];
