54d1e81de6a230f2c19d1b1265faa4b2278d7227
kanechu <kanechu@itdept.com.hk>
2014-08-27 16:37:12 +0800

    add new color

--- ittracking/Res_color.h
+++ ittracking/Res_color.h
@@ -23,6 +23,7 @@
 
 #define COLOR_AQUA [UIColor colorWithRed:0 green:255.0/255.0 blue:255.0/255.0 alpha:1]
 #define COLOR_PINK [UIColor colorWithRed:234.0/255.0 green:191.0/255.0 blue:229.0/255.0 alpha:1.0]
+#define COLOR_LIGHT_GREEN [UIColor colorWithRed:0 green:255.0/255.0 blue:0 alpha:1.0]
 
 @interface Res_color : NSObject
 
