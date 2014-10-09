9f09c69e6f2185d76add600571ec4f4bf93f3950
kanechu <kanechu@itdept.com.hk>
2014-10-09 12:26:47 +0800

    fix -add new path

--- ittracking/AppConstants.h
+++ ittracking/AppConstants.h
@@ -10,12 +10,15 @@
 #define DEFAULT_USERCODE @"ANONYMOUS"
 #define DEFAULT_PASS @"ANONYMOUS1"
 #define DEFAULT_SYSTEM @"ITNEW"
+#define DEFAULT_COMPANY_CODE @"WTRANLOG"
 @interface AppConstants : NSObject
 
 extern NSString* const STR_BASE_URL;
+extern NSString* const STR_LOGIN_URL;
+extern NSString* const STR_SYPARA_URL;
+extern NSString* const STR_MILESTONE_IMAGE_URL;
 extern NSString* const STR_SEA_URL;
 extern NSString* const STR_AIR_URL;
-extern NSString* const STR_LOGIN_URL;
 extern NSString* const STR_ALERT_URL;
 extern NSString* const STR_MILESTONE_URL;
 extern NSString* const STR_SCHEDULE_URL;
@@ -23,4 +26,5 @@ extern NSString* const STR_PORTNAME_URL;
 extern NSString* const STR_SEARCHCRITERIA_URL;
 extern NSString* const STR_ICON_URL;
 extern NSString* const STR_EXCNTR_STATUS_URL;
+
 @end
--- ittracking/AppConstants.m
+++ ittracking/AppConstants.m
@@ -9,18 +9,21 @@
 #import "AppConstants.h"
 
 #ifdef DEBUG
-NSString* const STR_BASE_URL = @"http://223.255.167.158/";
-NSString* const STR_SEA_URL =@"itwebapi/api/cargotracking/exhbl";
-NSString* const STR_AIR_URL =@"itwebapi/api/cargotracking/aehbl";
-NSString* const STR_LOGIN_URL=@"itwebapi/api/users/login";
-NSString* const STR_ALERT_URL=@"itwebapi/api/cargotracking/alert";
-NSString* const STR_MILESTONE_URL=@"itwebapi/api/cargotracking/milestone";
-NSString* const STR_SCHEDULE_URL=@"itwebapi/api/cargotracking/schedule";
-NSString* const STR_PORTNAME_URL=@"itwebapi/api/master/maport";
-NSString* const STR_SEARCHCRITERIA_URL=@"itwebapi/api/search/searchcriteria";
-NSString* const STR_ICON_URL=@"itwebapi/api/system/icon";
-NSString* const STR_EXCNTR_STATUS_URL=@"itwebapi/api/cargotracking/excntr_status";
+NSString* const STR_BASE_URL = @"http://192.168.1.17/";
+NSString* const STR_LOGIN_URL=@"kie_web_api/api/users/login";
+NSString* const STR_SYPARA_URL=@"/kie_web_api/api/System/sypara";
+NSString* const STR_MILESTONE_IMAGE_URL=@"kie_web_api/api/CargoTracking/milestoneimage";
+NSString* const STR_SEA_URL =@"kie_web_api/api/cargotracking/exhbl";
+NSString* const STR_AIR_URL =@"kie_web_api/api/cargotracking/aehbl";
+NSString* const STR_ALERT_URL=@"kie_web_api/api/cargotracking/alert";
+NSString* const STR_MILESTONE_URL=@"kie_web_api/api/cargotracking/milestone";
+NSString* const STR_SCHEDULE_URL=@"kie_web_api/api/cargotracking/schedule";
+NSString* const STR_PORTNAME_URL=@"kie_web_api/api/master/maport";
+NSString* const STR_SEARCHCRITERIA_URL=@"kie_web_api/api/search/searchcriteria";
+NSString* const STR_ICON_URL=@"kie_web_api/api/system/icon";
+NSString* const STR_EXCNTR_STATUS_URL=@"kie_web_api/api/cargotracking/excntr_status";
 #else
+//ITDEPT COMPANY
 NSString* const STR_BASE_URL = @"http://demo.itdept.com.hk/";
 NSString* const STR_SEA_URL =@"itleo.web/api/cargotracking/exhbl";
 NSString* const STR_AIR_URL =@"itleo.web/api/cargotracking/aehbl";
@@ -32,4 +35,20 @@ NSString* const STR_PORTNAME_URL=@"itleo.web/api/master/maport";
 NSString* const STR_SEARCHCRITERIA_URL=@"itleo.web/api/search/searchcriteria";
 NSString* const STR_ICON_URL=@"itleo.web/api/system/icon";
 NSString* const STR_EXCNTR_STATUS_URL=@"itleo.web/api/cargotracking/excntr_status";
+
+//Worldtrans company
+NSString* const STR_BASE_URL = @"http://223.255.167.158/";
+NSString* const STR_LOGIN_URL=@"itwebapi/api/users/login";
+NSString* const STR_SYPARA_URL=@"itwebapi/api/System/sypara";
+NSString* const STR_MILESTONE_IMAGE_URL=@"";
+NSString* const STR_SEA_URL =@"itwebapi/api/cargotracking/exhbl";
+NSString* const STR_AIR_URL =@"itwebapi/api/cargotracking/aehbl";
+NSString* const STR_ALERT_URL=@"itwebapi/api/cargotracking/alert";
+NSString* const STR_MILESTONE_URL=@"itwebapi/api/cargotracking/milestone";
+NSString* const STR_SCHEDULE_URL=@"itwebapi/api/cargotracking/schedule";
+NSString* const STR_PORTNAME_URL=@"itwebapi/api/master/maport";
+NSString* const STR_SEARCHCRITERIA_URL=@"itwebapi/api/search/searchcriteria";
+NSString* const STR_ICON_URL=@"itwebapi/api/system/icon";
+NSString* const STR_EXCNTR_STATUS_URL=@"itwebapi/api/cargotracking/excntr_status";
+
 #endif
