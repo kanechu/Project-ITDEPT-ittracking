abcaa1d7b0229d31ce13fe85c467832e0abf81cf
kanechu <kanechu@itdept.com.hk>
2014-12-27 12:12:31 +0800

    fix -alert

--- ittracking/AehblGeneralController.m
+++ ittracking/AehblGeneralController.m
@@ -248,14 +248,14 @@ enum ROW_NUMOFSECTION {
 -(void)fn_timeOut_handle{
     if (_flag_isTimeout!=2) {
         [MBProgressHUD hideHUDForView:self.view animated:YES];
-        UIAlertView *alertView=[[UIAlertView alloc]initWithTitle:nil message:@"Network requests data timeout !" delegate:self cancelButtonTitle:@"Retry" otherButtonTitles:@"Cancel", nil];
+        UIAlertView *alertView=[[UIAlertView alloc]initWithTitle:nil message:@"Network requests data timeout !" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Retry", nil];
         [alertView show];
         _flag_isTimeout=1;
     }
 }
 #pragma mark -UIAlertViewDelegate
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
-    if (buttonIndex==0) {
+    if (buttonIndex!=[alertView cancelButtonIndex]) {
         CheckNetWork *check_obj=[[CheckNetWork alloc]init];
         if ([check_obj fn_isPopUp_alert]==NO) {
             [self fn_get_data:is_search_column :is_search_value];
